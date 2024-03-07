9343cfd81abd22a43e6b258afda3c571b0fd4cb7
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-03-07 22:32:12 +0700

    add color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import type { Config } from 'tailwindcss'
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#026D9F',
+        'second': '#F8A31F'
+      },
+    },
   },
   plugins: [],
 } satisfies Config
