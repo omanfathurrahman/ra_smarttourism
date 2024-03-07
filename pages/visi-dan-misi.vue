6ec3727893fe15e9f6368fc12e3930da29a1c5eb
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-03-07 22:31:44 +0700

    adding image to the html

--- pages/visi-dan-misi.vue
+++ pages/visi-dan-misi.vue
@@ -1,14 +1,9 @@
 <template>
-  <div class="px-[6rem] bg-green-600">
+  <div class="px-[6rem]">
     <Header />
-    <div class="h-[calc(100dvh-4.5rem)] bg-orange-300"></div>
+    <div class="h-[calc(100dvh-4.5rem)] flex items-center justify-center">
+      <img src="~/assets/Union.png" alt="" class="w-full">
+    </div>
+    <div class="absolute w-full h-[20rem] bg-primary top-0 left-0 -z-10"></div>
   </div>
 </template>
-
-<script lang="ts" setup>
-
-</script>
-
-<style>
-
-</style>
