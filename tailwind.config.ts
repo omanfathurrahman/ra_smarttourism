e74de36bcaf8f37ed087e82fdb5223f7cffc401d
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-03-10 18:04:40 +0700

    style: add color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,8 @@ export default {
     extend: {
       colors: {
         'primary': '#026D9F',
-        'second': '#F8A31F'
+        'second': '#F8A31F',
+        'third': '#EC018C',
       },
     },
   },
