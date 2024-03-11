ce2960e60ebc652da76578c1e4d7b812e61cea3a
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-03-11 09:32:28 +0700

    feat: Add pages for Inovasi, Bisnis and mbkm

---
+++ pages/inovasi.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="">
+    <div class="px-24">
+      <Header />
+      <div
+        class="absolute w-full h-[24rem] bg-primary top-0 left-0 -z-10"
+      ></div>
+      <div class="h-[calc(100dvh-4.5rem)] flex items-center justify-center relative -z-10">
+        <img src="~/assets/visi-dan-misi.png" alt="" class="w-full" />
+        <h1
+          class="absolute top-1/2 translate-y-[-50%] left-1/2 translate-x-[-50%] text-6xl font-semibold"
+        >
+          Inovasi
+        </h1>
+        <div class="w-[20rem] h-[2rem] bg-orange-400 absolute top-[3.3rem]"></div>
+      </div>
+      <div class="border-b pb-10 border-zinc-700 relative">
+        <h2 class="text-black text-6xl font-semibold">
+          Inovasi Pada Coe Smart Tourism and Hospitality
+        </h2>
+        <p class="text-black text-justify font-thin text-2xl mt-8">
+          Di dunia yang terus berubah, industri pariwisata dan perhotelan
+          memerlukan inovasi yang cerdas dan progresif untuk memimpin jalan ke
+          depan. Pusat Keunggulan (COE) Smart Tourism dan Hospitality hadir
+          sebagai wahana bagi kolaborasi, eksperimen, dan pemikiran kreatif
+          dalam menghadapi tantangan dan peluang di era digital ini.
+        </p>
+        <p class="text-black text-justify font-thin text-2xl mt-4">
+          Dalam COE ini, kami mengundang Anda untuk memasuki dunia inovasi yang
+          tak terbatas, di mana teknologi, kreativitas, dan kebijaksanaan
+          bertemu untuk menciptakan pengalaman wisata yang lebih cerdas, lebih
+          berkelanjutan, dan lebih memikat bagi pengunjung dari seluruh dunia.
+        </p>
+        <div class="flex justify-start mt-20">
+          <div class="size-[7rem] bg-third rounded-tr-full"></div>
+        </div>
+      </div>
+      <div class="pb-24 pt-8">
+        <div class="text-black mt-12">
+          <h2 class="text-6xl font-semibold">
+            Beberapa Hasil Inovasi Yang Telah CoE Smart Tourism & Hospitality
+            Lakukan
+          </h2>
+          <p class="w-[90%] text-2xl font-thin text-balance mt-6">
+            Terdapat beberapa inovasi yang telah kami lakukan untuk dapat
+            mengembangkan pariwisata dan perhotelan.
+          </p>
+        </div>
+        <div class="grid grid-cols-2 gap-24 py-20">
+          <div class="relative" v-for="i in 2">
+            <div class="aspect-[3/2] overflow-hidden">
+              <img
+                src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                class="w-full h-full object-cover"
+              />
+            </div>
+            <div
+              class="w-[16rem] h-[2.8rem] bg-second absolute -top-6 z-10 left-1/2 -translate-x-1/2"
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
---
+++ pages/mbkm-dan-magang.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="">
+    <div class="px-24 pb-28">
+      <Header />
+      <div
+        class="absolute w-full h-[24rem] bg-primary top-0 left-0 -z-10"
+      ></div>
+      <div class="h-[calc(100dvh-4.5rem)] flex items-center justify-center relative -z-10">
+        <img src="~/assets/visi-dan-misi.png" alt="" class="w-full" />
+        <h1
+          class="absolute top-1/2 translate-y-[-50%] left-1/2 translate-x-[-50%] text-6xl font-semibold"
+        >
+          MBKM dan Magang
+        </h1>
+        <div class="w-[20rem] h-[2rem] bg-orange-400 absolute top-[3.3rem]"></div>
+      </div>
+      <div class="pb-10 relative">
+        <h2 class="text-black text-6xl font-semibold">
+          MBKM dan Magang Pada Coe Smart Tourism and Hospitality
+        </h2>
+        <p class="text-black text-justify font-thin text-2xl mt-8">
+          Kami dengan bangga memperkenalkan Program Mata Kuliah Berbasis Magang
+          (MBKM) dan Program Magang di Pusat Keunggulan (COE) Smart Tourism dan
+          Hospitality. Program ini adalah kesempatan berharga bagi para
+          mahasiswa dan profesional muda untuk mendapatkan pengalaman praktis
+          yang berharga dalam industri pariwisata dan perhotelan yang berkembang
+          pesat.
+        </p>
+        <p class="text-black text-justify font-thin text-2xl mt-4">
+          Kami berkomitmen untuk memberikan pengalaman yang bermakna dan
+          bermanfaat bagi setiap peserta program MBKM dan Magang kami. Dengan
+          bimbingan mentor yang berpengalaman dan lingkungan kerja yang
+          mendukung, kami yakin bahwa setiap peserta akan mengambil langkah
+          pertama yang penting dalam perjalanan mereka menuju kesuksesan dalam
+          industri pariwisata dan hospitality.
+        </p>
+        <div
+          class="size-[22rem] rounded-full bg-second absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[36%] -z-10"
+        />
+      </div>
+    </div>
+    <div class="px-24 bg-primary">
+      <div class="py-16">
+        <div class="w-8/12 mx-auto relative">
+          <img
+            src="https://images.unsplash.com/photo-1600880292203-757bb62b4baf?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            class="w-full h-full object-cover aspect-[3/2]"
+          />
+          <div
+            class="w-[18rem] h-[3rem] bg-second absolute top-[-1.4rem] left-1/2 -translate-x-1/2"
+          ></div>
+        </div>
+        <div class="mt-20">
+          <h3 class="text-6xl italic font-semibold">Pengenalan</h3>
+          <p class="mt-4 text-2xl font-thin text-justify">
+            Program MBKM dan Magang kami dirancang untuk menyatukan teori dengan
+            praktik, memberikan kesempatan bagi para peserta untuk
+            mengaplikasikan pengetahuan yang diperoleh di kelas ke dalam
+            lingkungan kerja yang nyata. Dengan berpartisipasi dalam program
+            ini, mahasiswa tidak hanya akan memperluas wawasan mereka tentang
+            konsep-konsep kunci dalam industri pariwisata dan hospitality,
+            tetapi juga akan mengembangkan keterampilan praktis yang diperlukan
+            untuk berhasil dalam karir mereka.
+          </p>
+        </div>
+      </div>
+      <div class="h-[1px] w-full bg-white"></div>
+      <div class="py-16">
+        <h3 class="text-6xl font-semibold">
+          Program MBKM dan Magang Dalam Bidang CoE Smart Tourism And Hospitality
+        </h3>
+        <div class="mt-16 flex flex-col gap-20">
+          <div class="flex gap-10 odd:flex-row even flex-row-reverse" v-for="i in 2">
+            <div class=" w-[55%]">
+              <h4 class="text-3xl font-semibold">Pengembangan Website untuk Pariwisata</h4>
+              <p class="text-lg font-thin mt-6 text-justify">
+                Mahasiswa dapat mengambil kursus yang melibatkan pembelajaran
+                tentang pengembangan aplikasi mobile yang spesifik untuk
+                industri pariwisata. Mereka kemudian dapat melakukan magang di
+                perusahaan perangkat lunak atau agen perjalanan untuk merancang
+                dan mengembangkan aplikasi mobile yang memudahkan wisatawan
+                dalam merencanakan perjalanan, mendapatkan informasi tentang
+                destinasi wisata, dan memesan layanan terkait.
+              </p>
+            </div>
+            <div class="relative w-[45%]">
+              <img
+                src="https://images.unsplash.com/photo-1600880292203-757bb62b4baf?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                class="w-full h-full object-cover aspect-[2/1]"
+              />
+              <div
+                class="w-[18rem] h-[2.3rem] bg-second absolute top-[-1.4rem] left-1/2 -translate-x-1/2"
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
