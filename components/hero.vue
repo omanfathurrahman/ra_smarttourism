9a6a5f7bdc052583f15645c4d94ca74729153fe5
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-03-15 10:44:09 +0700

    refactor: Add hero component

---
+++ components/hero.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    imgFile: string;
+    title: string;
+  }>();
+</script>
+
+<template>
+  <div class="relative w-full -z-10">
+    <NuxtImg :src="`img/${props.imgFile}`" alt="" class="w-full h-full" />
+    <h1 class="absolute w-full top-1/2 translate-y-[-80%] left-1/2 translate-x-[-50%] text-6xl font-semibold text-center">
+      {{ props.title }}
+    </h1>
+    <div class="w-[20rem] h-[2rem] bg-orange-400 absolute -top-3 left-1/2 translate-x-[-50%]"></div>
+  </div>
+</template>
