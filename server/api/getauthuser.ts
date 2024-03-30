463d5d1c82a33cfca252cf20d8de56a50c5eab95
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-03-31 01:01:38 +0700

    feat✨: membuat api untuk membuat signature, mengambil user saat ini, dan login

--- server/api/getauthuser.ts
+++ server/api/getauthuser.ts
@@ -1,5 +1,7 @@
-import { prisma } from "~/prisma/db";
+import { prisma } from "~/prisma/db"
 
 export default defineEventHandler(async (event) => {
-  return prisma.user.findUnique({ where: { id: event.context.auth.id } })
-});
+  if (event.context.auth) {
+    return prisma.user.findUnique({ where: { id: event.context.auth.id } })
+  }
+})
