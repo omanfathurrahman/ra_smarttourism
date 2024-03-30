d9eaa4c49ae60d21c8ab3294bc8bec04f663bf3a
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-03-31 00:42:26 +0700

    feat✨: buat component description atau what we did untuk bagian admin

---
+++ components/article/descriptionOrWhatWeDid/list.vue
@@ -0,0 +1,24 @@
+<script
+  lang="ts"
+  setup
+>
+  const props = defineProps<{
+    data: { style: string, items: string[] }
+  }>()
+  function removeLineBreaks(text: string): string {
+    // Replace all occurrences of <br> with an empty string
+    return text.replace(/<br\s*\/?>/gi, '');
+}
+</script>
+
+<template>
+  <ul class="">
+    <li
+      v-for="item in props.data.items"
+      :key="item"
+      class="list-disc list-inside"
+    >
+      {{ removeLineBreaks(item) }}
+    </li>
+  </ul>
+</template>
---
+++ components/article/descriptionOrWhatWeDid/paragraf.vue
@@ -0,0 +1,17 @@
+<script
+  setup
+  lang="ts"
+>
+  const props = defineProps<{
+    data: { text: string }
+  }>()
+
+  function removeNonBreakingSpace(text: string): string {
+    // Replace all occurrences of &nbsp; with a regular space
+    return text.replace(/&nbsp;/g, ' ');
+  }
+</script>
+
+<template>
+  <p>{{ removeNonBreakingSpace(data.text) }}</p>
+</template>
