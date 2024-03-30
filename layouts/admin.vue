565c7a95a0c8a9bc565657a64c1b466f77f94c24
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-03-31 00:47:18 +0700

    feat✨: buat layout untuk bagian admin

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,21 +1,31 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
 <template>
-  <div>
-    <p>admin</p>
-    <slot />
+  <div class="bg-gradient-to-br from-[#1c92d2] to-[#bef5fe] p-3 min-h-screen">
+    <div class="bg-white min-h-[calc(100vh-24px)] rounded-lg flex flex-col py-2">
+      <AdminHeader />
+      <div class="flex flex-auto">
+        <AdminSidebar class="w-2/12" v-if="route.path != '/admin'" />
+        <div class="bg-gradient-to-br from-transparent from-30% to-slate-50 px-6 py-3" :class="route.path != '/admin'?'w-10/12': 'w-full'">
+          <slot />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <style>
-.page-enter-active,
+/* .page-enter-active,
 .page-leave-active {
   transition: all 0.4s;
 }
 
 .page-enter-from,
 .page-leave-to {
-  /* opacity: 0; */
   filter: blur(1rem);
-  transform: translateY(3rem);
-}
+  background-color: white;
+} */
 </style>
